3090d6ea6581bd1ca63e5efa66e7547313c753d7
oleksandr.o.morozov <morozovaleks@ukr.net>
2021-12-11 20:37:20 +0200

    add internet permission and basic structure packages

--- app/src/main/java/com/example/testtaskforbootcamp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/testtaskforbootcamp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.testtaskforbootcamp
+package com.example.testtaskforbootcamp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testtaskforbootcamp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
